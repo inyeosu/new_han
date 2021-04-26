641938e57f9b9bfef21cdb932775ea4793bbb3c8
한승태 <sthan8832@gmail.com>
2021-04-26 16:23:52 +0900

    404_only

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -22,8 +22,8 @@ def server_error(e):
     return render_template('500.html'), 500
 
 
-def page_not_found(e):
-    return render_template('404.html'), 404
+# def page_not_found(e):
+#    return render_template('404.html'), 404
 
 
 def create_app():
@@ -58,6 +58,6 @@ def create_app():
 
     # 오류 페이지
     app.register_error_handler(404, page_not_found)
-    app.register_error_handler(500, server_error)
+    # app.register_error_handler(500, server_error)
 
     return app
